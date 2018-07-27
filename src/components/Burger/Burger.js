ca5de973e497bafd1c722ebac9305582361a5ef7
jatinkashyap <jatinkashyap9@gmail.com>
2018-07-26 22:46:38 -0400

    July26commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) =>{
+
+    let newIngredients= Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,i) => {
+            return <BurgerIngredient key={igKey+i} type={igKey}/>
+        })
+    }).reduce((arr,el) => {
+        return arr.concat(el)
+    },[]);
+
+    if(newIngredients.length === 0){
+        newIngredients = <div> Please add something to burger </div>;
+    }
+
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {newIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+}
+export default burger;
